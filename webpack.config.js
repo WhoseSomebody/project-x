6fb0cbfd3b48843d1397acfb96805880b98936e4
Vitalii Dominichenko <gard@Gard-Mac.local>
2018-09-29 10:22:48 +0300

    fixed build and added Procfile. Try 1

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './Client/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'Client', 'build'),
     publicPath: '/'
   },
   module: {

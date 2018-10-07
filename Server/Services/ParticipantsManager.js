0d5b7873d27feaeaba2f49f94275c3668d654a05
Vitalii Dominichenko <gard@Gard-Mac.local>
2018-10-07 15:48:07 +0300

    Added phone to db

--- Server/Services/ParticipantsManager.js
+++ Server/Services/ParticipantsManager.js
@@ -16,6 +16,7 @@ class ParticipantManager {
       height,
       weight,
       email,
+      phone,
       qualificationLink,
       photoBase64,
       // image
@@ -36,6 +37,7 @@ class ParticipantManager {
           height,
           weight,
           email,
+          phone,
           qualificationLink,
           imageUrl
         })

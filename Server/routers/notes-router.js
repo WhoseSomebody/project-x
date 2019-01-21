565d109c1e55853d2f3a66c501ba431a889ee6e0
Vitalii Dominichenko <rdchampionship@gmail.com>
2019-01-21 15:36:06 +0200

    Secured.

--- Server/routers/notes-router.js
+++ Server/routers/notes-router.js
@@ -3,6 +3,7 @@
 // package references
 const express = require('express');
 var formidable = require('formidable');
+var jwt = require('jwt-simple');
 
 // app references
 const EmailManager = require('../Services/EmailManager');
@@ -12,6 +13,19 @@ const ParticipantsManager = require('../Services/ParticipantsManager');
 const emailManager = new EmailManager();
 const particiantsManager = new ParticipantsManager();
 
+const getToken = () => {
+  const payload = new Date()
+    .toJSON()
+    .slice(0, 10)
+    .split('')
+    .reverse()
+    .join('')
+    .replace(/-/g, '');
+  const secret = toString(Math.PI * 10000000);
+  console.log(secret);
+  const token = jwt.encode(payload, secret);
+  return token;
+};
 // build router
 
 const notesRouter = () => {
@@ -28,14 +42,16 @@ const notesRouter = () => {
     })
     .post('/participant', (req, res) => {
       var form = new formidable.IncomingForm();
+
+      if (req.get('Token') !== getToken()) res.status(400).send({ error: 'Wrong token' });
       // let formFields = {};
       return form.parse(req, function(err, fields) {
         if (err) res.status(500).send(err);
 
         return particiantsManager
           .addParticipant(fields)
-          .then(id => res.status(201).send({ id: id }))
-          .catch(error => {
+          .then((id) => res.status(201).send({ id: id }))
+          .catch((error) => {
             console.log(error);
             res.status(500).send(error);
           });
@@ -43,29 +59,65 @@ const notesRouter = () => {
     })
     .delete('/participant/:id', (req, res) => {
       const { id } = req.params;
-      const someToken =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0b3B0YWwuY29tIiwiZXhwIjoxNDI2NDIwODAwLCJodHRwOi8vdG9wdGFsLmNvbS9qd3RfY2xhaW1zL2lzX2FkbWluIjp0cnVlLCJjb21wYW55IjoiVG9wdGFsIiwiYXdlc29tZSI6dHJ1ZX0.yRQYnWzskCZUxPwaQupWkiUzKELZ49eM7oWxAQK_ZXw';
 
-      if (!id || req.get('Token') !== someToken) {
+      if (!id || req.get('Token') !== getToken()) {
         res.status(400).end();
       } else {
         particiantsManager
           .removeParticipant(id)
           .then(() => res.status(200).send('Participant deleted'))
-          .catch(error => {
+          .catch((error) => {
             console.log(error.message);
             res.status(500).send();
           });
       }
     })
     .get('/list-participants', (req, res) => {
+      const showFullData = req.get('Token') === getToken();
       return particiantsManager
         .listParticipants(req.query)
-        .then(participants => res.json(participants))
-        .catch(error => {
+        .then((data) => {
+          if (showFullData) return res.json(data);
+          const { participants, count, pages, page, limit } = data;
+          return res.json({
+            participants: participants.map(
+              ({ _id, name, surname, age, height, weight, qualificationLink, instagramLink, imageUrl }) => ({
+                _id,
+                name,
+                surname,
+                age,
+                height,
+                weight,
+                qualificationLink,
+                instagramLink,
+                imageUrl,
+              })
+            ),
+            count,
+            pages,
+            page,
+            limit,
+          });
+        })
+        .catch((error) => {
           console.log(error);
           res.status(500).send(error);
         });
+    })
+    .post('/login', (req, res) => {
+      const { login, password } = req.body;
+
+      if (login === 'rdwcTeam2019' && password === '@N*verG1veUp,Dude:)') {
+        const token = getToken();
+        return res.json({
+          token,
+        });
+      } else return res.status(400).send({ error: 'Wrong login & password.' });
+    })
+    .post('/is-logged-in', (req, res) => {
+      const isLoggedIn = req.get('Token') === getToken();
+      if (isLoggedIn) return res.status(200).end();
+      else return res.status(400).end();
     });
 
   return router;
